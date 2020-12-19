dd5529c9e531d3494178f0bb60922b2542d421c0
Omar Shahid <omarshahid160@gmail.com>
2020-12-19 22:25:23 +0500

    Added auto blur functionality when search form is submitted

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Loader from "./components/Loader";
 import SearchForm from "./components/SearchForm";
 import ShipsList from "./components/ShipsList";
 import { useAllShipsQuery } from "./generated/graphql";
 
 function App() {
-  const [searchStr, setSearchStr] = useState("")
+  const [searchStr, setSearchStr] = useState("");
   const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchStr(e.target.value)
-  }
+    setSearchStr(e.target.value);
+  };
 
-  const {loading, data, error} = useAllShipsQuery();
+  const { loading, data, error } = useAllShipsQuery();
   return (
     <div className="p-4">
-      <h1 className="text-8xl text-gray-700 text-center font-extralight">SpaceX CLient (Ships)</h1>
+      <h1 className="text-8xl text-gray-700 text-center font-extralight">
+        SpaceX CLient (Ships)
+      </h1>
       <p>{loading ? <Loader /> : ""}</p>
-      <SearchForm onSearchStr={onSearchChange}/>
+      <SearchForm onSearchStr={onSearchChange} />
       {!loading && !error && <ShipsList searchStr={searchStr} ships={data!} />}
       {error && <h1>Oops, an error occured</h1>}
     </div>
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 
-interface Props{
-  onSearchStr: (e: React.ChangeEvent<HTMLInputElement>) => void
+interface Props {
+  onSearchStr: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SearchForm: React.FC<Props> = ({onSearchStr}) => (
-    <div className="w-9/12 mx-auto mt-8">
-      <form>
-        <input onChange={onSearchStr} type="text" name="" id="" placeholder="Search..." className="mx-auto text-gray-600 w-full text-xl p-3 border-gray-400 border-b-2 focus:outline-none focus:border-blue-400" />
-        <input type="submit" name="search" value="" />
-      </form>
-    </div>
-    
-  );
-
-
-
+const SearchForm: React.FC<Props> = ({ onSearchStr }) => (
+  <div className="w-9/12 mx-auto mt-8">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <input
+        onChange={onSearchStr}
+        type="text"
+        name=""
+        id=""
+        onKeyDown={(e) =>
+          e.key === "Enter" && (e.target as HTMLInputElement).blur()
+        }
+        placeholder="Search..."
+        className="mx-auto text-gray-600 w-full text-xl p-3 border-gray-400 border-b-2 focus:outline-none focus:border-blue-400"
+      />
+      <input type="submit" name="search" value="" />
+    </form>
+  </div>
+);
 
 export default SearchForm;
