8023c36af8217868a827ad490d60aac1ce1fdbdd
Omar Shahid <omarshahid160@gmail.com>
2020-12-19 22:42:28 +0500

    Added purge in tailwind config for optimized build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
   presets: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
